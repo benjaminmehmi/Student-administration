d6f8e21d45695a404d42e5883e23e7476816dbb3
Benjamin Mehmi <benjaminmehmi@hotmail.com>
2025-11-28 10:44:48 +0100

    Init

---
+++ Studentadministration/Program.cs
@@ -0,0 +1,10 @@
+﻿using Studentadministration;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var school = new School();
+        school.Run();
+    }
+}
---
+++ Studentadministration/Subjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Studentadministration
+{ public class SubjectInfo
+    {
+            public string Subject { get; private set; }
+            public int SubjectCode { get; private set; }
+            public int StudyPoints { get; private set; }
+
+
+            public SubjectInfo(string subject, int subjectcode, int studypoints)
+            {
+                Subject = subject;
+                SubjectCode = subjectcode;
+                StudyPoints = studypoints;
+            }
+        
+    }
+}
+
